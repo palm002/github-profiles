b1e86691ca59964809d9d2ad5c9c0121a8edb0ff
Mirko <mirkopalancaji@gmail.com>
2020-02-18 13:47:07 +1100

    Perform and handle multicurrent requests

--- src/components/App.js
+++ src/components/App.js
@@ -4,22 +4,46 @@ import axios from 'axios';
 import SearchBar from './SearchBar';
 import PageView from './PageView';
 
-
 class App extends React.Component {
-  state = { user: [] }
+  state = { user: {}, repos: [] };
 
   onSearchSubmit = async user => {
-    const response = await axios.get(`https://api.github.com/users/${user}`)
-  
-    console.log(response.data);
-    this.setState({ user: response.data })
+    await axios
+      .all([
+        axios.get(`https://api.github.com/users/${user}`),
+        axios.get(`https://api.github.com/users/${user}/repos`)
+      ])
+      .then(
+        axios.spread((userRes, repoRes) => {
+          // completed requests
+          // TODO: confirm if this is the correct way to do things or outside of promises
+          this.setState({ user: userRes.data, repos: repoRes.data });
+        })
+      )
+      .catch(error => {
+        // catch & handle error here
+        console.log(error);
+
+        if (error.response) {
+          // show friendly msg to user
+          console.log(error.response.data);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+        } else if (error.request) {
+          // this block is for when there is no response coming back
+          console.log(error.response);
+        } else {
+          // other error -- in the setting up the request
+          console.log(error.message)
+        }
+      });
   };
 
   render() {
     return (
       <div className="container-fluid">
         <SearchBar onSubmit={this.onSearchSubmit} />
-        <PageView user={this.state.user} />
+        <PageView user={this.state.user} repos={this.state.repos} />
       </div>
     );
   }
