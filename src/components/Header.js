7a115406888f5f8ba0437df47b97c92a8a980a7d
Mirko <mirkopalancaji@gmail.com>
2020-02-18 16:33:36 +1100

    Add icons

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Header = () => {
-  return <h2 style={{textAlign: `center`}}>Github Profiles</h2>;
+  return <h2 style={{ textAlign: `center` }}>Github Profiles</h2>;
 };
 
 export default Header;
