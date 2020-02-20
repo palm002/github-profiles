1cd78895d21c59523c163d7f1c439fff7c79fb7f
Mirko <mirkopalancaji@gmail.com>
2020-02-20 16:32:00 +1100

    Added styling

--- src/components/PageView.js
+++ src/components/PageView.js
@@ -4,8 +4,8 @@ import UserInfo from './UserInfo';
 import RepoInfo from './RepoInfo';
 
 const PageView = props => (
-  <div className="row">
-    <div className="col-md-6">
+  <div className="row justify-content-around mx-auto my-3">
+    <div className="col-md-3">
       <UserInfo user={props.user} />
     </div>
     <div className="col-md-6">
--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,15 +1,25 @@
 import React from 'react';
 
 const UserInfo = props => {
-  const { avatar_url, login, followers, public_repos } = props.user;
+  const { avatar_url, login, name, followers, public_repos } = props.user;
 
-  //   TODO: styling
+  // TODO: add borders/background colors
   return (
-    <div className="" style={{ textAlign: `center` }}>
-      <img style={{ height: `250px` }} src={avatar_url} alt="" />
-      <h6>{login}</h6>
-      <p>{followers && <> {followers} Followers </>}</p>
-      <p>{public_repos && <> {public_repos} Repositories </>} </p>
+    <div className="profile">
+      <div style={{ textAlign: `center` }}>
+        <img
+          className="img-fluid shadow-sm my-1"
+          src={avatar_url}
+          alt=""
+        />
+        <h6 className="py-2">{name}</h6>
+      </div>
+    
+      <div className="row justify-content-around">
+        <p className="px-1 py-1">{login}</p>
+        <p className="px-1 py-1">{followers && <> {followers} Followers </>}</p>
+        <p className="px-1 py-1">{public_repos && <> {public_repos} Repositories </>} </p>
+      </div>
     </div>
   );
 };
